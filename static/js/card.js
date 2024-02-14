eb8e7968b7c9b0e4c89ec7655396485280c45897
Dest <firefoxdest@gmail.com>
2024-02-14 11:03:28 +0300

    fix css v2

--- static/js/card.js
+++ static/js/card.js
@@ -6,7 +6,7 @@ function hide() {
     var buttons_div = document.getElementById("hide");
     buttons_div.style.display = "none";
     var image = document.querySelector('#image');
-    image.src = "../static/video/cat4.webp";
+    image.src = "static/video/cat4.webp";
     var text = document.querySelector('#text');
     text.innerHTML = "Text"
   }
